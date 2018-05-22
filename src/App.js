6b7d751302b9ab21f89eb7e508c024e0c0436f96
ajCastiglione <antoniocastiglione10@gmail.com>
2018-05-22 09:03:09 -0400

    moved ajax request back to weather component due to promise complications

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Weather from './components/weather';
+import Weather from './components/weatherForcast';
 
 class App extends Component {
   render() {
