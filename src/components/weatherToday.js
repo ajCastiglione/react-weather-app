5627f28d7a6e941008c77ef49fe04ab4f730da46
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-08-25 11:36:00 -0400

    Fixed humidity rounding issue, fixed location issue where coords weren't being passed down

--- src/components/weatherToday.js
+++ src/components/weatherToday.js
@@ -1,89 +1,124 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
-import { key } from './apiKey';
+import React, { Component } from "react";
+import $ from "jquery";
+import { key } from "./apiKey";
 
 class WeatherToday extends Component {
+  state = {
+    today:
+      sessionStorage.today !== undefined
+        ? JSON.parse(sessionStorage.today)
+        : [],
+    fetchForcast: false,
+    lat:
+      localStorage.coords !== undefined
+        ? JSON.parse(localStorage.coords)[0]
+        : this.props.lat,
+    long:
+      localStorage.coords !== undefined
+        ? JSON.parse(localStorage.coords)[1]
+        : this.props.long
+  };
 
-    state = {
-        today: (sessionStorage.today !== undefined ? JSON.parse(sessionStorage.today) : []),
-        fetchForcast: false,
-        lat: localStorage.coords !== undefined ? JSON.parse(localStorage.coords)[0] : this.props.lat,
-        long: localStorage.coords !== undefined ? JSON.parse(localStorage.coords)[1] : this.props.long
+  componentDidMount() {
+    setInterval(this.getToday, 1000 * 300);
+  }
+
+  componentDidUpdate() {
+    if (
+      this.props.getForcast === true &&
+      this.state.fetchForcast !== this.props.getForcast
+    ) {
+      if (sessionStorage.today !== undefined) return;
+      this.setState({ fetchForcast: this.props.getForcast });
+      this.getToday();
     }
-
-    componentDidMount() {
-        setInterval(this.getToday, 1000 * 300);
+    if (this.props.shouldIUpdate === true) {
+      this.getToday();
+      this.props.changeStatus();
     }
+  }
 
-    componentDidUpdate() {
-        if(this.props.getForcast === true && this.state.fetchForcast !== this.props.getForcast) {
-            if(sessionStorage.today !== undefined) return;
-            this.setState({ fetchForcast: this.props.getForcast});
-            this.getToday();
-        }
-        if(this.props.shouldIUpdate === true) {
-            this.getToday();
-            this.props.changeStatus();        
-        }
-    }
+  getToday = () => {
+    $.ajax({
+      url: `https://api.darksky.net/forecast/${key}/${this.props.lat},${
+        this.props.long
+      }?exlude=minutely?units=uk2`,
+      dataType: "JSONP",
+      type: "GET",
+      success: function(r) {
+        let today = r.currently;
+        let unixConvert = new Date(today.time * 1000),
+          day = unixConvert.getDay(),
+          temp = today.temperature,
+          summary = today.summary,
+          wind = today.windSpeed,
+          humidity = today.humidity;
+        let days = [
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday"
+        ];
 
-    getToday = () => {
-        $.ajax({
-            url: `https://api.darksky.net/forecast/${key}/${this.state.lat},${this.state.long}?exlude=minutely?units=uk2`,
-            dataType: 'JSONP',
-            type: 'GET',
-            success: function(r) {
-                let today = r.currently;
-                let unixConvert = new Date(today.time * 1000), day = unixConvert.getDay(), temp = today.temperature, summary = today.summary, wind = today.windSpeed, humidity = today.humidity;
-                let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-                this.setState({ today: [{day: days[day], temp: temp, summary: summary, wind: wind, humidity: humidity}] });
-                sessionStorage.today = JSON.stringify(this.state.today);
-                this.props.changeText();
-            }.bind(this),
-            error: function (xhr, status, error) {
-                console.error(xhr, status, error);
+        this.setState({
+          today: [
+            {
+              day: days[day],
+              temp: temp,
+              summary: summary,
+              wind: wind,
+              humidity: humidity
             }
+          ]
         });
-    }
+        sessionStorage.today = JSON.stringify(this.state.today);
+        this.props.changeText();
+      }.bind(this),
+      error: function(xhr, status, error) {
+        console.error(xhr, status, error);
+      }
+    });
+  };
 
-
-    render() {
-        const local = this.props.location;
-        const today = this.state.today;
-        return (
-            <section className="today-container">
-                <div className="container">
-                        {
-                            this.state.today.length !== 0 && sessionStorage.today !== null ?
-                            <div className="today-content col-xs-12">
-                                <div className="inner-today-content">
-                                    <div className="weather-data">
-                                        <p className="weather-single-temp">{Math.round(today[0].temp)} <sup>&deg;F</sup></p>
-                                        <p className="weather-single-summary">{today[0].summary}</p>
-                                        <div className="weather-single-humidity">
-                                            <p>humidity</p>
-                                            <p>{today[0].humidity * 100}%</p>
-                                        </div>
-                                        <div className="weather-single-wind">
-                                            <p>wind</p>
-                                            <p>{today[0].wind} MPH</p>
-                                        </div>
-                                    </div>
-                                    <div className="location-data">
-                                        <p className="weather-single-location">{local}</p>
-                                        <p className="weather-single-day">{ today[0].day }</p>
-                                    </div>
-                                </div>
-                            </div>
-                            :
-                            <p>Loading...</p>
-                            
-                        }
+  render() {
+    const local = this.props.location;
+    const today = this.state.today;
+    return (
+      <section className="today-container">
+        <div className="container">
+          {this.state.today.length !== 0 && sessionStorage.today !== null ? (
+            <div className="today-content col-xs-12">
+              <div className="inner-today-content">
+                <div className="weather-data">
+                  <p className="weather-single-temp">
+                    {Math.round(today[0].temp)} <sup>&deg;F</sup>
+                  </p>
+                  <p className="weather-single-summary">{today[0].summary}</p>
+                  <div className="weather-single-humidity">
+                    <p>humidity</p>
+                    <p>{Math.round(today[0].humidity * 100)}%</p>
+                  </div>
+                  <div className="weather-single-wind">
+                    <p>wind</p>
+                    <p>{today[0].wind} MPH</p>
+                  </div>
                 </div>
-            </section>
-        )
-    }
+                <div className="location-data">
+                  <p className="weather-single-location">{local}</p>
+                  <p className="weather-single-day">{today[0].day}</p>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default WeatherToday;
